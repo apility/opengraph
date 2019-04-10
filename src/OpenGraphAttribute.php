fa2d83e68a819ef832f159ded38ce702842250ad
Tommy Stigen Olsen <tommysolsen@gmail.com>
2019-04-10 15:23:55 +0200

    Implement an OpenGraphAttribute interface

---
+++ src/OpenGraphAttribute.php
@@ -0,0 +1,14 @@
+<?php
+namespace Apility\OpenGraph;
+
+interface OpenGraphAttribute {
+  /**
+   * This function returns an OpenGraphAttribute in array form
+   * 
+   * The output should look like this recursive data structure
+   * [value, [OpenGraphAttribute]]
+   * 
+   * @return array
+   */
+  function openGraphNode(): array;
+}
